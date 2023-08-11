a95dd8445a1574b85bb733955b91c89cedab7ea1
Quazi Minhajuddin <minhajqazi@live.com>
2023-08-11 15:36:22 +0530

    test

---
+++ app.js
@@ -0,0 +1,8 @@
+function render() {
+  document.getElementById("demo").innerHTML =
+    "<img src='https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_01_thum.jpg' />";
+}
+
+window.onload = () => {
+  render();
+};
--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("M1")
-	fmt.Println("M2")
-	fmt.Println("M3")
-	fmt.Println("F1")
-	fmt.Println("F1.1")
-	fmt.Println("F2")
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	// Set CORS headers to allow requests from any origin
+	// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// 	fmt.Println("helllo")
+	// 	htmlContent := `
+	// 		document.getElementById('demo').innerHTML = "<img src='https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_01_thum.jpg' />"
+	// 	`
+	// 	w.Header().Set("Content-Type", "text/html")
+	// 	fmt.Fprint(w, htmlContent)
+	// })
+
+	// fmt.Println("Server started on :8080")
+	// http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hello")
+		// 	// Set CORS headers to allow requests from any origin
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		http.ServeFile(w, r, "app.js")
+	})
+
+	fmt.Println("Server started on :8080")
+	http.ListenAndServe(":8080", nil)
 }
